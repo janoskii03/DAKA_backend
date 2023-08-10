63d14b030a7d0ff2b5cc6605b4c992219090e057
EllieMon <elliemon2020@gmail.com>
2023-08-10 16:08:34 +0800

    後台座位切版V1

---
+++ src/components/seats/Date.vue
@@ -0,0 +1,47 @@
+<template>
+    <Space size="large" wrap class="reservation_date_time">
+        <DatePicker size="large" type="date" placeholder="Select date" v-model="selectedDate" @mouseleave="convertDate" />
+        <TimePicker size="large" type="timerange" :steps="[1, 60, 60]"  placement="bottom-end" placeholder="Select time" style="width: 168px" v-model="selectedTime" @mouseleave="convertTime"
+        />
+      </Space>
+
+
+</template>
+<style>
+
+.reservation_date_time{
+  display: flex;
+  justify-content: center;
+ padding:0 50px;
+  
+
+}
+
+</style>
+<script>
+
+export default {
+
+
+
+data(){
+  
+  return{
+    selectedDate:'',
+    selectedTime:'',
+  }
+},
+computed(){
+  
+
+},
+methods:{
+convertDate(){
+  this.$emit('convert-date',this.selectedDate);
+},
+convertTime(){
+  this.$emit('convert-time',this.selectedTime);
+}
+}
+}
+</script>
